5fcc95c3e9769fca01c15d7d91cb70b3ece5263d
L <2944083996@qq.com>
2025-07-15 20:24:09 +0800

    Initial Commit

---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by mart S on 2025/7/15.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
