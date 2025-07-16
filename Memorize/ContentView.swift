25ec8fa7d8efd67c00ef276f5e369c50010f8997
L <2944083996@qq.com>
2025-07-16 19:23:03 +0800

    cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+            // 也可循环实现
+            // 不要让函数和计算变量的代码超过20行，最好12行
         }
+        .foregroundColor(.orange)
         .padding()
+        
+    }
+}
+
+// 橙色描边的卡片
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12) // 无法同时填充白色和描边
+                    .stroke(lineWidth: 2)   //.stroke(style: StrokeStyle(lineWidth: 10, dash: [10,2]))
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12) // 没有指定颜色所以外面的颜色会渗透进来
+            }
+            
+        }
     }
 }
 
